24a679372d39529485465a68cb64ed9b568e6497
ychenxastra <154291107+ychenxastra@users.noreply.github.com>
2024-01-08 17:12:19 -0500

    Update nb_author_id.py

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -26,29 +26,16 @@ features_train, features_test, labels_train, labels_test = preprocess()
 # Enter Your Code Here
 from sklearn.naive_bayes import GaussianNB
 clf = GaussianNB()
+t0 = time()
 clf.fit(features_train, labels_train)
+print("Training Time:", round(time()-t0, 3), "s")
 
+t0 = time()
 pred = clf.predict(features_test)
+print("Predicting Time:", round(time()-t0, 3), "s")
 
 from sklearn.metrics import accuracy_score
 accuracy = accuracy_score(pred, labels_test)
 
 print(accuracy)
-##############################################################
-
-##############################################################
-'''
-You Will be Required to record time for Training and Predicting 
-The Code Given on Udacity Website is in Python-2
-The Following Code is Python-3 version of the same code
-'''
-
-# t0 = time()
-# # < your clf.fit() line of code >
-# print("Training Time:", round(time()-t0, 3), "s")
-
-# t0 = time()
-# # < your clf.predict() line of code >
-# print("Predicting Time:", round(time()-t0, 3), "s")
-
 ##############################################################
