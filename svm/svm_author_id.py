f8493c9d3027591241d51d7058f68a610a42b90b
ychenxastra <154291107+ychenxastra@users.noreply.github.com>
2024-01-09 14:31:44 -0500

    Update svm_author_id.py

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -18,22 +18,27 @@ from email_preprocess import preprocess
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
-
+# features_train = features_train[:int(len(features_train)/100)]
+# labels_train = labels_train[:int(len(labels_train)/100)]
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+clf = SVC(kernel="rbf",C=10000)
 
+t0 = time()
+clf.fit(features_train, labels_train)
+print("Training Time:", round(time()-t0, 3), "s")
 
-#########################################################
+t0 = time()
+pred = clf.predict(features_test)
+print("Predicting Time:", round(time()-t0, 3), "s")
 
-#########################################################
-'''
-You'll be Provided similar code in the Quiz
-But the Code provided in Quiz has an Indexing issue
-The Code Below solves that issue, So use this one
-'''
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred, labels_test)
+print(acc)
 
-# features_train = features_train[:int(len(features_train)/100)]
-# labels_train = labels_train[:int(len(labels_train)/100)]
+# How many of them are Chris' emails?
+print("Number of Chris' emails:", sum(pred))
 
 #########################################################
