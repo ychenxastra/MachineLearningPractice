1700cb0832365e3e3ba97b61b79cf2387df05f6d
ychenxastra <154291107+ychenxastra@users.noreply.github.com>
2024-01-12 18:17:32 -0500

    Add files via upload

--- feature_selection/find_signature.py
+++ feature_selection/find_signature.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
+from time import time
 import joblib
 import numpy
+import pickle
 numpy.random.seed(42)
 
 
@@ -10,10 +12,8 @@ numpy.random.seed(42)
 ### mini-project.
 words_file = "../text_learning/your_word_data.pkl" 
 authors_file = "../text_learning/your_email_authors.pkl"
-word_data = joblib.load( open(words_file, "r"))
-authors = joblib.load( open(authors_file, "r") )
-
-
+word_data = pickle.load( open(words_file, "rb"))
+authors = pickle.load( open(authors_file, "rb") )
 
 ### test_size is the percentage of events assigned to the test set (the
 ### remainder go into training)
@@ -23,7 +23,8 @@ from sklearn.model_selection import train_test_split
 features_train, features_test, labels_train, labels_test = train_test_split(word_data, authors, test_size=0.1, random_state=42)
 
 from sklearn.feature_extraction.text import TfidfVectorizer
-vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5, stop_words='english')
+vectorizer = TfidfVectorizer(sublinear_tf=True, 
+	max_df=0.5, stop_words='english')
 features_train = vectorizer.fit_transform(features_train)
 features_test  = vectorizer.transform(features_test).toarray()
 
@@ -35,8 +36,53 @@ features_train = features_train[:150].toarray()
 labels_train   = labels_train[:150]
 
 
-
 ### your code goes here
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
+clf = tree.DecisionTreeClassifier()
+t0 = time()
+clf.fit(features_train, labels_train)
+print("training time:", round(time()-t0, 3), "s")
 
+t0 = time()
+pred = clf.predict(features_test)
+print("predicting time:", round(time()-t0, 3), "s")
+acc = accuracy_score(pred, labels_test)
+print("accuracy_score", acc)
 
+# What’s the importance of the most important feature?
+# What is the number of this feature?
+importances = clf.feature_importances_
+for index, item in enumerate(importances):
+    if item > 0.2:        
+        print(index, item)
+
+import numpy as np
+indices = np.argsort(importances)[::-1]
+print('Feature Ranking: ')
+for i in range(10):
+    print("{} feature no.{} ({})".format(i+1,indices[i],importances[indices[i]]))
+
+# What’s the most powerful word when your decision tree is makeing its classification decisions?
+feature_name = vectorizer.get_feature_names_out()
+for index, item in enumerate(feature_name):
+    if index == 33614:        
+        print(item)
+
+vectorizer.get_feature_names_out()[33614]
+# Result: sshacklensf      
+        
+feature_name = vectorizer.get_feature_names_out()
+for index, item in enumerate(feature_name):
+    if index == 14343:        
+        print(item) 
+        
+vectorizer.get_feature_names_out()[14343]
+# Result: cgermannsf
+        
+feature_name = vectorizer.get_feature_names_out()
+for index, item in enumerate(feature_name):
+    if index == 14343:        
+        print(item) 
+# Result: houectect
