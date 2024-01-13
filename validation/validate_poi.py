68730217bb3bd27dfbb2b83a5b6351145e56d73c
ychenxastra <154291107+ychenxastra@users.noreply.github.com>
2024-01-12 22:37:51 -0500

    Add files via upload

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -14,6 +14,7 @@ import joblib
 import sys
 sys.path.append(os.path.abspath("../tools/"))
 from feature_format import featureFormat, targetFeatureSplit
+from sklearn.model_selection import train_test_split
 
 data_dict = joblib.load(open("../final_project/final_project_dataset.pkl", "rb") )
 
@@ -28,5 +29,19 @@ labels, features = targetFeatureSplit(data)
 
 
 ### it's all yours from here forward!  
+from sklearn import tree
+
+clf = tree.DecisionTreeClassifier()
+clf.fit(features, labels)
+clf.score(features, labels)
+
+print(clf.score(features, labels))
 
 
+features_train, features_test, labels_train, labels_test = train_test_split(features,
+                labels,test_size=0.3,random_state=42)
+clf = tree.DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+clf.score(features_test, labels_test)
+
+print(clf.score(features_test, labels_test))
